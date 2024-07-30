c8d51679656ee6da662c3271e1e6d5ad160fd15c
Ahmed Abd Elghani <30578096+ahmedabdulghany7@users.noreply.github.com>
2024-07-30 22:52:00 +0300

    Delete Frequency counting using an Array.py

--- Frequency counting using an Array.py
+++
@@ -1,8 +0,0 @@
-lst = list(map(int,input().split()))
-freq = 770*[0]
-for i in lst:
-    index = i + 500
-    freq [index] +=1
-for index, count in enumerate(freq):
-    if count > 0:
-        print('Value', index - 500, 'appeared', count, 'times')
